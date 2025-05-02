4e6d40d503994ef30fb208fc0e7845b349499df5
belousmind <belousmind@gmail.com>
2025-05-02 13:11:36 +0300

    fix: modal window closing animation

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useEffect } from 'react'
 import styles from './modal.module.scss'
+import { motion, AnimatePresence } from 'framer-motion'
 
 type ModalProps = {
   isOpen: boolean
@@ -29,14 +30,31 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
     }
   }, [isOpen, onClose])
 
-  if (!isOpen) return null
-
   return (
-    <div className={styles.overlay} onClick={onClose}>
-      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-        {children}
-      </div>
-    </div>
+    <>
+      <AnimatePresence mode="wait">
+        {isOpen && (
+          <motion.div
+            className={styles.overlay}
+            onClick={onClose}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <motion.div
+              className={styles.modal}
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0, opacity: 0 }}
+              transition={{ duration: 0.2, ease: 'easeOut' }}
+              onClick={(e) => e.stopPropagation()}
+            >
+              {children}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
 
